6601d09db2e694a8b13500a621235b4e376c69f7
Andrés Quiceno <asquicenoh97@elpuig.xeill.net>
2019-05-09 20:00:11 +0200

    Initial commit

---
+++ app/src/main/java/com/example/realm1/MyApplication.java
@@ -0,0 +1,21 @@
+package com.example.realm1;
+
+import android.app.Application;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //Iniciem el realm
+        Realm.init(this);
+        RealmConfiguration config = new RealmConfiguration.Builder()
+                .name("realm.realm")
+                .schemaVersion(0)
+                .migration(new Migration())
+                .build();
+        Realm.setDefaultConfiguration(config);
+    }
+}
